cdc361affe48b9135b9312a4e9f207178bd589e3
jenkins <jenkins@edna.ru>
2023-05-05 13:44:16 +0300

    Release 1.2.9

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "EDNAPushXCE",
-            url: "https://github.com/edna-io/push-x-ce-ios/releases/download/1.2.8/edna-push-xce-1.2.8.zip",
-            checksum: "a4cb966d73dc88a96d93e21389382e63605b144b6b46a65b9216393162d8f339"
+            url: "https://github.com/edna-io/push-x-ce-ios/releases/download/1.2.9/edna-push-xce-1.2.9.zip",
+            checksum: "755128149d466c6d2111a4fa7026e51ec2dee5727e889b36d7c7f58ce02ed03a"
         ),
     ]
 )
